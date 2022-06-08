dcb3ef39e27a19d6ca797f8b13e34b1bc1a91b33
st21233 <105762906+st21233@users.noreply.github.com>
2022-06-09 10:52:21 +1200

    Component.1 customer full name

--- Julie's item tracker.py
+++ Julie's item tracker.py
@@ -7,14 +7,30 @@ root = Tk()
 
 
 
-# print details of all items
-def print_camp_details():
-    global j_names, total_entries, name_count
-    name_count = 0
-    Label(main_window, font='bold', text="Customer full name").grid(column=0, row=7)
-    Label(main_window, font='bold', text="receipt number").grid(column=1, row=7)
-    Label(main_window, font='bold', text="hired item").grid(column=2, row=7)
-    Label(main_window, font='bold', text="number of hired items").grid(column=3, row=7)
+
+#Customer Full name
+from tkinter import *
+title = Label(root, text="Julie's Item Tracker",
+              font=(("verdana"), 15))
+name_txt = Label(root, text="Customer full name :")
+name_input = Entry(root,width=30)
 
 
-mainloop()
+# create button
+button = Button(root, text='Save')
+
+# Use Geometry Manager to show our widgets
+title.place(x=100, y=20) # x and y are in pixels - and that
+# distance from the edges of the window.
+# Run you rapplication to see the placement of the widget.
+
+
+
+# Placement of Components
+name_txt.place(x=20, y=80)
+name_input.place(x=100, y=80)
+
+
+root.geometry("500x250")
+
+root.mainloop()
