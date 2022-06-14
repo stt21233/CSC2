0cd1ef6c755612305b18ce73ca2ee1076b26bdcc
st21233 <105762906+st21233@users.noreply.github.com>
2022-06-14 14:16:19 +1200

    Create a print camp details
    and
    
    callback for main

--- Julie's item tracker.py
+++ Julie's item tracker.py
@@ -1,140 +1,158 @@
+# import tkinter so we can make a GUI
 from tkinter import *
-root = Tk()
-
-
-
-# Button: Quit Program
-def quit():
-    main.window.destroy()
-
-
-
 
 # Program Titles
-title = Label(root, text="Julie's Item Tracker",
-              font=(("verdana"), 15))
-title.place(x=100, y=20)
-subh1 = Label(root, text="Data Collection",
-              font=(("verdana"), 13))
-subh1.place(x=20, y=60)
 
 
-
-# Customer Full name
-name_txt = Label(root, text="full name :")
-name_input = Entry(root,width=30)
-
-# Receipt Number
-receipt_txt = Label(root, text="Receipt number :")
-receipt_input = Entry(root,width=30)
-
-# Hired Items
-hireditem_txt = Label(root, text="Hired item :")
-hireditem_input = Entry(root,width=30)
-
-# number of hired item
-total_txt = Label(root, text="Number of hired item :")
-total_input = Entry(root,width=30)
-
-# delete row #
-delete_txt = Label(root, text="Delete row #")
-delete_input = Entry(root, width=30)
-
-# Placement of Components
-     # Program Title
-title.place(x=100, y=13)
-     # Subheading - Data Collection
-subh1.place(x=35, y=60)
-     # customer full name - Component.1
-name_txt.place(x=20, y=80)
-name_input.place(x=15, y=100)
-     # Receipt number - Component.2
-receipt_txt.place(x=20, y=120)
-receipt_input.place(x=15, y=140)
-     # Hired item - Component.3
-hireditem_txt.place(x=20, y=160)
-hireditem_input.place(x=15, y=180)
-     # Number of hired item - Component.4
-total_txt.place(x=20, y=200)
-total_input.place(x=15, y=220)
-     # Delete row # -
-delete_txt.place(x=20, y=240)
-delete_input.place(x=15, y=260)
-
-
-
-
-# create button
-button = Button(root, text='Save')
-#-----
+def quit():
+    main_window.destroy()
 
 # print details of all the camps
+
+# print camp detail (when press print all details will show)
 def print_camp_details():
-    global name_input, receipt_input, hireditem_input, total_input
+    # these are the global variables that are used
+    global entry_name, entry_receiptnumber, entry_hireditem, entry_totalhired
     name_count = 0
-    Label(main_window, font='bold', text="Customer Full Name").grid(x=100, y=15)
-    Label(main_window, font='bold', text="Receipt Number").grid(x=120, y=7)
-    Label(main_window, font='bold', text="Hired item").grid(x=140, y=19)
-    Label(main_window, font='bold', text="Total hired items").grid(x=150, y=70)
-
-
+    # Create the column headings
+    Label(main_window, font=("Helvetica 10 bold"),
+          text="full name").grid(column=0, row=7)
+    Label(main_window, font=("Helvetica 10 bold"),
+          text="receipt number").grid(column=1, row=7)
+    Label(main_window, font=("Helvetica 10 bold"),
+          text="item hired").grid(column=2, row=7)
+    Label(main_window, font=("Helvetica 10 bold"),
+          text="number of hired item").grid(column=3, row=7)
+    Label(main_window, font=("Helvetica 10 bold") )
+          
+# add each item in the list into its own row
     while name_count < total_entries:
-        Label(main_window, text=name_count).grid(column=0, row=name_count + 8)
-        Label(main_window, text=(camp_details[name_count][0])).grid(column=1, row=name_count + 8)
-        Label(main_window, text=(camp_details[name_count][1])).grid(column=2, row=name_count + 8)
-        Label(main_window, text=(camp_details[name_count][2])).grid(column=3, row=name_count + 8)
-        Label(main_window, text=(camp_details[name_count][3])).grid(column=4, row=name_count + 8)
+        Label(main_window, text=name_count).grid(column=0, row=name_count+8)
+        Label(main_window, text=(camp_details[name_count][0])).grid(
+            column=1, row=name_count+8)
+        Label(main_window, text=(camp_details[name_count][1])).grid(
+            column=2, row=name_count+8)
+        Label(main_window, text=(camp_details[name_count][2])).grid(
+            column=3, row=name_count+8)
+        Label(main_window, text=(camp_details[name_count][3])).grid(
+            column=4, row=name_count+8)
         name_count += 1
 
+# Check the inputs are all valid
+
+
+def check_inputs():
+    # these are the global variables that are used
+    global camp_details, entry_leader, entry_location, entry_campers, entry_weather, total_entries
+    input_check = 0
+    Label(main_window, text="               ") .grid(column=2, row=0)
+    Label(main_window, text="               ") .grid(column=2, row=1)
+    Label(main_window, text="               ") .grid(column=2, row=2)
+    Label(main_window, text="               ") .grid(column=2, row=3)
+    # Check that leader is not blank, set error text if blank
+    if len(entry_leader.get()) == 0:
+        Label(main_window, fg="red", text="Required") .grid(column=2, row=0)
+        input_check = 1
+    # Check that location is not blank, set error text if blank
+    if len(entry_location.get()) == 0:
+        Label(main_window, fg="red", text="Required") .grid(column=2, row=1)
+        input_check = 1
+    # Check the number of campers is not blank and between 5 and 10, set error text if blank
+    if (entry_campers.get().isdigit()):
+        if int(entry_campers.get()) < 5 or int(entry_campers.get()) > 10:
+            Label(main_window, fg="red", text="5-10 only") .grid(column=2, row=2)
+            input_check = 1
+    else:
+        Label(main_window, fg="red", text="5-10 only") .grid(column=2, row=2)
+        input_check = 1
+    # Check that weather is not blank, set error text if blank
+    if len(entry_weather.get()) == 0:
+        Label(main_window, fg="red", text="Required") .grid(column=2, row=3)
+        input_check = 1
+    if input_check == 0:
+        append_name()
 
 # add the next camper to the list
+
+# create button - append details
 def append_name():
-    global camp_details, entry_leader, entry_location, entry_campers, entry_weather, total_entries
-    if len(entry_leader.get()) != 0:
-        camp_details.append([entry_leader.get(), entry_location.get(), entry_campers.get(), entry_weather.get()])
-        entry_leader.delete(0, 'end')
-        entry_location.delete(0, 'end')
-        entry_campers.delete(0, 'end')
-        entry_weather.delete(0, 'end')
-        total_entries += 1
+    # these are the global variables that are used
+    global camp_details, entry_name, entry_location, entry_campers, entry_weather, total_entries
+    # append each item to its own area of the list
+    camp_details.append([entry_leader.get(), entry_location.get(),
+                         entry_campers.get(), entry_weather.get()])
+    # clear the boxes
+    entry_leader.delete(0, 'end')
+    entry_location.delete(0, 'end')
+    entry_campers.delete(0, 'end')
+    entry_weather.delete(0, 'end')
+    total_entries += 1
+
+# delete a row from the list
 
 
-
-#julie button
-def setup_buttons():
-    global camp_details, name_input, receipt_input,total_input, delete_input
-    Button(main_window, text="Quit program", command=quit.grid(x=5, y=200))
-    Button(main_window, txt="Append Details", command=append_name).grid(x=40, y=100)
-    Label(main_window, text="Delete Row #").grid(x=100, y=200)
-    delete_item = Entry(main_window)
-    delete_item.grid(column=1, row=6)
-    Button(main_window, text="Delete", command=delete_row).grid(column=2, row=6)
-
-#delete a row from the list
 def delete_row():
+    # these are the global variables that are used
     global camp_details, delete_item, total_entries, name_count
+    # find which row is to be deleted and delete it
     del camp_details[int(delete_item.get())]
     total_entries = total_entries - 1
     delete_item.delete(0, 'end')
-    Label(main_window, text="       ").grid(column=0, row=name_count + 7)
-    Label(main_window, text="       ").grid(column=1, row=name_count + 7)
-    Label(main_window, text="       ").grid(column=2, row=name_count + 7)
-    Label(main_window, text="       ").grid(column=3, row=name_count + 7)
-    Label(main_window, text="       ").grid(column=4, row=name_count + 7)
+    # clear the last item displayed on the GUI
+    Label(main_window, text="       ").grid(column=0, row=name_count+7)
+    Label(main_window, text="       ").grid(column=1, row=name_count+7)
+    Label(main_window, text="       ").grid(column=2, row=name_count+7)
+    Label(main_window, text="       ").grid(column=3, row=name_count+7)
+    Label(main_window, text="       ").grid(column=4, row=name_count+7)
+    # print all the items in the list
     print_camp_details()
 
+# create the buttons and labels
+
+
+def setup_buttons():
+    # these are the global variables that are used
+    global camp_details, entry_leader, entry_location, entry_campers, entry_weather, total_entries, delete_item
+    # create all the empty and default labels, buttons and entry boxes. Put them in the correct grid location
+    Label(main_window, text="boss") .grid(column=0, row=0, sticky=E)
+    entry_leader = Entry(main_window)
+    entry_leader.grid(column=1, row=0)
+    Label(main_window, text="Location") .grid(column=0, row=1, sticky=E)
+    entry_location = Entry(main_window)
+    entry_location.grid(column=1, row=1)
+    Button(main_window, text="Quit", command=quit,
+           width=10) .grid(column=4, row=0, sticky=E)
+    Button(main_window, text="Append Details",
+           command=check_inputs) .grid(column=3, row=1)
+    Button(main_window, text="Print Details", command=print_camp_details,
+           width=10) .grid(column=4, row=1, sticky=E)
+    Label(main_window, text="No of Campers") .grid(column=0, row=2, sticky=E)
+    entry_campers = Entry(main_window)
+    entry_campers.grid(column=1, row=2)
+    Label(main_window, text="Weather") .grid(column=0, row=3, sticky=E)
+    entry_weather = Entry(main_window)
+    entry_weather.grid(column=1, row=3)
+    Label(main_window, text="Row #") .grid(column=3, row=2, sticky=E)
+    delete_item = Entry(main_window)
+    delete_item .grid(column=4, row=2)
+    Button(main_window, text="Delete Row", command=delete_row,
+           width=10) .grid(column=4, row=3, sticky=E)
+    Label(main_window, text="               ") .grid(column=2, row=0)
+
 # start the program running
+
+
 def main():
+    # these are the global variables that are used
     global main_window
     global camp_details, entry_name, entry_age, entry_gender, total_entries
+    # create empty list for camp details and empty variable for entries in the list
     camp_details = []
     total_entries = 0
+    # create the GUI and start it up
     main_window = Tk()
     setup_buttons()
     main_window.mainloop()
 
 
-
-root.geometry("660x300")
-
-root.mainloop()
+main()
