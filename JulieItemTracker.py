d1ca25cfa8a08ace63432a37edc0a048f6f72e0e
st21233 <105762906+st21233@users.noreply.github.com>
2022-06-16 11:27:28 +1200

    Version Control 2: Create Print details, and organize GUI like I planned for my outcome

--- JulieItemTracker.py
+++ JulieItemTracker.py
@@ -9,23 +9,23 @@ main_window = Tk()
 Label(main_window, text="Customer full name", font=("Helvetica bold", 9)).grid(row=1, column=1)
 entry_name = Entry(main_window)
 entry_name.grid(row=2, column= 1)
-Label(main_window, text="Receipt number", font=("Helvetica bold", 11)).grid(row=3, column=1)
+Label(main_window, text="Receipt number", font=("Helvetica bold", 9)).grid(row=3, column=1)
 entry_receipt = Entry(main_window)
 entry_receipt.grid(row=4, column=1)
-Label(main_window, text="Item hired", font=("Helvetica", 11)).grid(row=5, column=1)
+Label(main_window, text="Item hired", font=("Helvetica", 9)).grid(row=5, column=1)
 entry_hireditem = Entry(main_window)
 entry_hireditem.grid(row=6, column=1)
-Label(main_window, text="Number of hired items",font=("Helvetica", 11)).grid(row=7, column=1)
+Label(main_window, text="Number of hired items",font=("Helvetica", 9)).grid(row=7, column=1)
 entry_number_of_hired_item = Entry(main_window)
-entry_number_of_hired_item.grid(row=7, column=1)
-Label(main_window, text="Delete row #", font=("Helvetica", 11)).grid(row=7, column=1)
+entry_number_of_hired_item.grid(row=8, column=1)
+Label(main_window, text="Delete row #", font=("Helvetica", 9)).grid(row=9, column=1)
 entry_delete_row = Entry(main_window)
-entry_delete_row.grid(row=8, column=1)
+entry_delete_row.grid(row=10, column=1)
 # Create buttons
-Button(main_window, text="Append details", font=("Helvetica bold", 8)).grid(row=4,column=20)
-Button(main_window, text="Print details", font=("Helvetica bold", 8)).grid(row=5, column=21)
-Button(main_window, text="Delete button",font=("Helvetica bold", 8)).grid(row=8, column=29)
-Button(main_window, text="Quit program", font=("Helvetica bold", 8)).grid(row=0, column=21)
+Button(main_window, text="Append details", font=("Helvetica bold", 8)).grid(row=7,column=2)
+Button(main_window, text="Print details", font=("Helvetica bold", 8)).grid(row=8, column=2)
+Button(main_window, text="Delete",font=("Helvetica bold", 8)).grid(row=10, column=2)
+Button(main_window, text="Quit program", font=("Helvetica bold", 8)).grid(row=0, column=40)
 
 
 # Print Tracker Details
@@ -56,9 +56,6 @@ def print_tracker_details():
         Label(main_window, text=(print_tracker_details[name_count][3])).grid(
             column=4, row=name_count+8)
         name_count += 1
-
-
-
 
 
 
