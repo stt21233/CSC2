8376fe8c19bffaeef52559d561c755ec1fee970e
st21233 <105762906+st21233@users.noreply.github.com>
2022-06-23 13:45:21 +1200

    Version Control 4.2: Testing and trailing input checker for receipt number and number of hired item. Then making sure that print details works, and adding row

--- JulieItemTracker.py
+++ JulieItemTracker.py
@@ -27,42 +27,44 @@ def setup_buttons():
     Button(main_window, text="Append details", font=("Helvetica bold", 8),
            command=check_inputs).grid(row=9,column=1)
     Button(main_window, text="Print details", font=("Helvetica bold", 8),
-           command=print_tracker_details()).grid(row=9, column=2)
+           command=print_tracker_details).grid(row=9, column=2)
     Button(main_window, text="Delete", font=("Helvetica bold", 8),
            command=delete_row).grid(row=12, column=2)
     Button(main_window, text="Quit program", font=("Helvetica bold", 8),
            command=quit).grid(row=0, column=40)
 
-# Append details
+# Check Inputs
 def check_inputs():
     global entry_name, entry_receipt, entry_hired_item, entry_number_of_hired_item, entry_delete_row, total_entries
     input_check = 0
-    Label(main_window, text="               ").grid(column=2, row=2)
-    Label(main_window, text="               ").grid(column=2, row=4)
-    Label(main_window, text="               ").grid(column=2, row=6)
-    Label(main_window, text="               ").grid(column=2, row=8)
+    Label(main_window, text="                        ").grid(column=2, row=2)
+    Label(main_window, text="                        ").grid(column=2, row=4)
+    Label(main_window, text="                        ").grid(column=2, row=6)
+    Label(main_window, text="                        ").grid(column=2, row=8)
     # Check that Customer full name is not blank, set error text if blank
     if len(entry_name.get()) == 0:
-        Label(main_window, fg="red", text="Requiredc1").grid(column=2, row=2)
+        Label(main_window, fg="red", text="Required") .grid(column=2, row=2)
         input_check = 1
     # Check the number of receipt number is not blank and is a integer, set error text if blank
     if (entry_receipt.get().isdigit()):
-        if int(entry_receipt.get()) < 100000000 or int(entry_receipt.get()) > 999999999:
-            Label(main_window, fg="red", text="numerical only").grid(column=2, row=4)
-            input_check = 1
-    else:
-        Label(main_window, fg="red", text="required").grid(column=2, row=4)
+        Label(main_window, fg="red", text="                 ").grid(column=2, row=4)
         input_check = 1
+    else:
+        Label(main_window, fg="red", text="Number Only").grid(column=2, row=4)
+        input_check = 1
+
      # Check that Item hired is not blank, set error text if blank
     if len(entry_hired_item.get()) == 0:
-        Label(main_window, fg="red", text="Requiredc1").grid(column=2, row=6)
+        Label(main_window, fg="red", text="Required").grid(column=2, row=6)
         input_check = 1
     # Check that number of hired is not blank and is between 1-500, set error text if blank
     if (entry_number_of_hired_item.get().isdigit()):
-        if int(entry_number_of_hired_item.get()) < 1 or int(entry_number_of_hired_item()) > 500:
-            Label(main_window, fg="red", text="Required Between 1-500").grid(column=2, row=8)
+        if int(entry_number_of_hired_item.get()) < 1 or int(entry_number_of_hired_item.get()) > 500:
+            Label(main_window, fg="red", text="between 1-500").grid(column=2, row=8)
+            input_check = 1
+    else:
+        Label(main_window, fg="red", text="Between 1-500").grid(column=2, row=8)
         input_check = 1
-
 
 
 def append_name():
@@ -76,7 +78,7 @@ def append_name():
     entry_receipt.delete(0, 'end')
     entry_hired_item.delete(0, 'end')
     entry_number_of_hired_item.delete(0, 'end')
-    total_entries += 1
+    total_entries
 
 def delete_row():
     # these are the global variables that are used
@@ -101,27 +103,30 @@ def print_tracker_details():
     name_count = 0
     # Create the column headings
     Label(main_window, font=("Helvetica 10 bold"),
-          text="Customer Full Name").grid(column=11, row=2)
+          text="Row").grid(column=12, row=2)
     Label(main_window, font=("Helvetica 10 bold"),
-          text="Receipt Number").grid(column=12, row=2)
+          text="Customer Full Name").grid(column=13, row=2)
     Label(main_window, font=("Helvetica 10 bold"),
-          text="Item Hired").grid(column=13, row=2)
+          text="Receipt Number").grid(column=14, row=2)
     Label(main_window, font=("Helvetica 10 bold"),
-          text="Number of Hired items").grid(column=14, row=2)
+          text="Item Hired").grid(column=15, row=2)
+    Label(main_window, font=("Helvetica 10 bold"),
+          text="Number of Hired items").grid(column=16, row=2)
     Label(main_window, font=("Helvetica 10 bold"))
 
 # add each item in the list into its own row
     while name_count < total_entries:
-        Label(main_window, text=name_count).grid(column=0, row=name_count+8)
-        Label(main_window, text=(print_tracker_details[name_count][0])).grid(
-            column=1, row=name_count+8)
-        Label(main_window, text=(print_tracker_details[name_count][1])).grid(
-            column=2, row=name_count+8)
-        Label(main_window, text=(print_tracker_details[name_count][2])).grid(
-            column=3, row=name_count+8)
-        Label(main_window, text=(print_tracker_details[name_count][3])).grid(
-            column=4, row=name_count+8)
+        Label(main_window, text=name_count).grid(column=12, row=name_count+8)
+        Label(main_window, text=(customer_details[name_count][0])).grid(
+            column=13, row=name_count+8)
+        Label(main_window, text=(customer_details[name_count][1])).grid(
+            column=14, row=name_count+8)
+        Label(main_window, text=(customer_details[name_count][2])).grid(
+            column=15, row=name_count+8)
+        Label(main_window, text=(customer_details[name_count][3])).grid(
+            column=16, row=name_count+8)
         name_count += 1
+
 
 
 # Create GUI and make sure entry boxes and button are appearing
